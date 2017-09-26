1cddbd94b058d9c918fe4d5a076b335d632e7275
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-26 15:52:33 -0400

    Episode 11, while temperature was being adjusted, heating items in core replaced ingredients with extra array

--- episode-11/replicator.rb
+++ episode-11/replicator.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Replicator
 
   # When the Enterprise calls Replicator.new, this method executes.
@@ -40,7 +41,7 @@ class Replicator
   # and then manually execute each method to ensure
   # it returns what's expect.
   def replicate(recipe)
-
+    # binding.pry
     # Setup an instance variable for the recipe
     # so that other methods can see what the recipe is
     @recipe = recipe
@@ -134,13 +135,14 @@ class Replicator
   # This adjusts the temperature of the glass to
   # what the recipes calls for.
   def adjust_temperature
-
+    # binding.pry
     # Abort if there is no glass inside the replicator.
     return unless glass_inside_replicator
 
     # Transport glass to the reactor where the temperature adjustment will take place.
     # If successful, @enterprise.reactor.core will now contain the glass
     # and @inside_replicator will no longer contain the glass.
+    # binding.pry
     transport_glass_to_reactor
 
 
@@ -165,7 +167,7 @@ class Replicator
       number_of_adjustments += 1
 
     end
-
+    # binding.pry
     # Transport glass from reactor back to inside the replicator.
     # If successful, @enterprise.reactor.core will now be empty
     # and @inside_replicator will once again contain the glass.
